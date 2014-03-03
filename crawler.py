e8b948197446c7785a88f68f1c46d17e61f80356
Jameson <jameson@voxy.com>
2014-03-03 14:13:50 -0500

    splitting up status and link fetcher

--- crawler.py
+++ crawler.py
@@ -3,6 +3,9 @@ import urllib2
 import httplib
 from bs4 import BeautifulSoup
 
+# global link set
+linkset = []
+
 
 def get_links(url):
 	connector = urllib2.urlopen(url)
@@ -12,6 +15,7 @@ def get_links(url):
 	
 	for html_tag in links:
 		link = html_tag.get('href', None)
+		linkset.append(link)
 		if link != None:
 			print link
 
@@ -21,10 +25,15 @@ def get_status(url):
 	resp = conn.getresponse()
 	print r1.status, r1.reason
 
+def find_the_status():
+	for linkers in linkset:
+		get_status(linkers)
+
 
 def main():
 	url = raw_input('What url would you like to run the crawler against? ')
 	get_links('http://www.' + url + '/')
+	find_the_status()
 
 
 
